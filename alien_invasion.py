d1ecddd28989e0e3a658ecfc5a0a4c49cbaa065c
patrick <patrick@mumo.cl>
2021-03-03 15:49:10 -0300

    Refactorización del screen y la imagen ship

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,5 +1,6 @@
 import sys, pygame
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
@@ -10,18 +11,28 @@ class AlienInvasion:
         self.settings = Settings()
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption('Alien Invasion')
-
+        self.ship = Ship(self)
+        
     def run_game(self):
         """Start the main loop for the game."""
         while True:
             #watch for keyboard and mouse events.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-            #Redraw the screen during each pass through the loop.
-            self.screen.fill(self.settings.bg_color)
-            #Make the most recently drawn screen visible.
-            pygame.display.flip()
+            self._check_events()
+            self._update_screen()
+           
+    def _check_events(self):
+        """Respond to keypresses and mouse events."""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+ 
+    def _update_screen(self):
+        #Redraw the screen during each pass through the loop.
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()            
+        #Make the most recently drawn screen visible.
+        pygame.display.flip()
+
 
 if __name__ == '__main__':
     #Make a game instance, and run the game.
